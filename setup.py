595369b7882c5684af9a4f70253580acb29ed66c
Jacob Ward <jacob.alan.ward@gmail.com>
2022-06-22 15:03:57 -0600

    setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="pyometer",
+      version="0.1",
+      description="Python 3 metrics",
+      license='MIT',
+      author="Jake Ward",
+      url='https://github.com/AthlosEducation/pyometer',
+      packages=['pyometer', 'pyometer.metric', 'pyometer.reporter'])
